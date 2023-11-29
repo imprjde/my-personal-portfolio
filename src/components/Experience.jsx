329d405a3225b527f7c9b7456d0d87bb2835d4b8
Prajwal <imprjde7@gmail.com>
2023-11-29 12:57:25 +0530

    CSS FIX

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -93,9 +93,10 @@ const Experience = () => {
     {
       id: 12,
       src: Mongoose,
-      size: "100px",
+      styles: "150px",
+      styling: "pt-3",
       title: "Mongoose JS",
-      style: "shadow-green-400",
+      style: "shadow-red-500",
     },
     {
       id: 13,
@@ -151,13 +152,18 @@ const Experience = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-          {techs.map(({ id, src, title, style, size }) => (
+          {techs.map(({ id, src, title, style, styles, styling }) => (
             <div
               key={id}
               className={`shadow-md hover:scale-105 duration-500  cursor-pointer py-2 rounded-lg ${style}`}
             >
-              <img src={src} alt="" className={`w-20 mx-auto`} />
-              <p className="mt-4 S">{title}</p>
+              <img
+                src={src}
+                alt=""
+                style={{ width: styles }}
+                className={`w-20 mx-auto `}
+              />
+              <p className={`mt-4 S ${styling}`}>{title}</p>
             </div>
           ))}
         </div>
